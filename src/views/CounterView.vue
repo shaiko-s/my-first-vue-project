6e1b45a070d112e040a190f194fad1303a9d5261
shaiko-s <145833407+shaiko-s@users.noreply.github.com>
2024-09-25 18:35:40 +0200

    Lesson 25

---
+++ src/views/CounterView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useCounterStore } from '@/stores/counterStore';
+
+let counter = useCounterStore();
+</script>
+
+<template>
+    <div>
+        <h1>{{ counter.count }}</h1>
+
+        <button @click="counter.increment()"
+                :disabled="!counter.remaining">Increment ({{ counter.remaining }} Remaining)</button>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,9 @@
 <script setup>
-import TabbableTextarea from '@/components/TabbableTextarea.vue';
-import { ref } from 'vue';
-
-let comment = ref('test value');
 
 </script>
 
 <template>
   <main>
-    <form action="">
-      <TabbableTextarea v-model="comment"
-                        style="width: 100%; height: 300px;" />
-    </form>
+
   </main>
 </template>
