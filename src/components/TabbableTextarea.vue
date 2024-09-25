ed46b06990748982e50abdfa4f6e0760dad423f5
shaiko-s <145833407+shaiko-s@users.noreply.github.com>
2024-09-25 17:35:36 +0200

    Lesson 21

---
+++ src/components/TabbableTextarea.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+    modelValue: String
+});
+
+let emit = defineEmits(['update:modelValue']);
+
+function onTabPress(e) {
+    // let textarea = ref(null);
+
+    // textarea.value.addEventListener("keydown", (e) => {
+    let textarea = e.target;
+
+    // if (e.keyCode === 9) {
+    let val = textarea.value,
+        start = textarea.selectionStart,
+        end = textarea.selectionEnd;
+
+    textarea.value = val.substring(0, start) + "\t" + val.substring(end);
+
+    textarea.selectionStart = textarea.selectionEnd = start + 1;
+
+    // e.preventDefault();
+    // }
+    // });
+}
+
+</script>
+
+<template>
+    <textarea @keydown.tab.prevent="onTabPress"
+              @keyup="$emit('update:modelValue', $event.target.value)"
+              v-text="modelValue" />
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,16 @@
 <script setup>
-import { ref, watch } from 'vue';
-import { useStorage } from '@/composables/useStorage';
+import TabbableTextarea from '@/components/TabbableTextarea.vue';
+import { ref } from 'vue';
 
-let food = useStorage('food', 'icecream');
-let age = useStorage('age', '48');
-
-let obj = useStorage('obj', { one: 'one' });
-
-setTimeout(() => {
-  obj.value.one = 'two';
-}, 2000);
+let comment = ref('test value');
 
 </script>
 
 <template>
   <main>
-    <p>
-      What is your favorite food? <input type="text"
-             v-model="food">
-    </p>
-
-    <p>
-      What is your age? <input type="text"
-             v-model="age">
-    </p>
+    <form action="">
+      <TabbableTextarea v-model="comment"
+                        style="width: 100%; height: 300px;" />
+    </form>
   </main>
 </template>
